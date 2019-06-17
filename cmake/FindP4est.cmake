a3e628d25672fc268d0f40348fcf2ca18d4e3c65
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2019-06-17 22:27:06 +0200

    Fix build process and dependencies

--- cmake/FindP4est.cmake
+++ cmake/FindP4est.cmake
@@ -8,10 +8,19 @@ find_path(P4EST_INCLUDE_DIR
           PATH_SUFFIXES include)
 
 find_library(P4EST_LIBRARIES
-             NAMES p4est sc
+             NAMES p4est
              HINTS ${P4EST_DIR}
              ENV LIBRARY_PATH
              PATH_SUFFIXES lib)
+
+find_library(SC_LIBRARIES
+             NAMES sc
+             HINTS ${P4EST_DIR}
+             ENV LIBRARY_PATH
+             PATH_SUFFIXES lib)
+
+list(APPEND P4EST_LIBRARIES ${SC_LIBRARIES})
+
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(P4EST
