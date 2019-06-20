22364c0e62baf2df915f9b56e4024660970c85e6
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2019-06-20 11:28:21 +0200

    Add tests for boundary and ghost cells
    Ensure boundary and ghost cells have an associated communication.

--- tests/test_all_ghost_cells_valid.cpp
+++ tests/test_all_ghost_cells_valid.cpp
@@ -18,7 +18,9 @@
  */
 
 /**
- * Tests if all ghost cells have at least one inner neighbor cell.
+ * Tests if all ghost cells have at least one inner neighbor cell,
+ * and ensures that each ghost and boundary cells have associated
+ * communications.
  */
 #define BOOST_TEST_MODULE ghost_cells_valid
 #include <boost/test/included/unit_test.hpp>
@@ -28,8 +30,58 @@
 #include <boost/mpi/environment.hpp>
 #include <repa/repa.hpp>
 
-static void test(repa::grids::ParallelLCGrid *grid)
+using CVBIt = std::vector<bool>::const_iterator;
+static bool all_true(const CVBIt &first, const CVBIt &last)
 {
+    return std::all_of(first, last, [](bool b) { return b; });
+}
+
+template <typename Container>
+bool contains(Container cont, typename Container::value_type value)
+{
+    return std::find(std::begin(cont), std::end(cont), value) != std::end(cont);
+}
+
+static void test_boundary_has_comm(repa::grids::ParallelLCGrid *grid)
+{
+    // Checks that boundary cells have an associated sent operation
+    auto check_is_send_cell = [](int c, const auto &gexds) {
+        return std::find_if(
+                   std::begin(gexds), std::end(gexds),
+                   [c](const auto &gexd) { return contains(gexd.send, c); })
+               != std::end(gexds);
+    };
+
+    auto gexds = grid->get_boundary_info();
+    for (int c = 0; c < grid->n_local_cells(); ++c) {
+        for (int j = 0; j < 27; ++j) {
+            int d = grid->cell_neighbor_index(c, j);
+
+            if (d >= grid->n_local_cells()) {
+                check_is_send_cell(c, gexds);
+            }
+        }
+    }
+}
+
+static void test_ghost_has_comm(repa::grids::ParallelLCGrid *grid)
+{
+    // Test that all ghost cells have an associated receive operation
+    std::vector<bool> used(grid->n_ghost_cells(), false);
+
+    for (const auto &g : grid->get_boundary_info()) {
+        for (int ghost : g.recv) {
+            // Each ghost cell can only have one receive operation.
+            BOOST_TEST(!used[ghost - grid->n_local_cells()]);
+            used.at(ghost - grid->n_local_cells()) = true;
+        }
+    }
+    BOOST_TEST(all_true(std::begin(used), std::end(used)));
+}
+
+static void test_ghost_has_local(repa::grids::ParallelLCGrid *grid)
+{
+    // Test that all ghost cells have a neighboring inner cell
     std::vector<bool> used(grid->n_ghost_cells(), false);
 
     for (int c = 0; c < grid->n_local_cells(); ++c) {
@@ -41,11 +93,14 @@ static void test(repa::grids::ParallelLCGrid *grid)
             }
         }
     }
-
-    auto all_true = [](auto first, auto last) {
-        return std::all_of(first, last, [](bool b) { return b; });
-    };
     BOOST_TEST(all_true(std::begin(used), std::end(used)));
+}
+
+static void test(repa::grids::ParallelLCGrid *grid)
+{
+    test_ghost_has_local(grid);
+    test_ghost_has_comm(grid);
+    test_boundary_has_comm(grid);
 }
 
 BOOST_AUTO_TEST_CASE(test_all_ghost_cells_valid)
