72cdb3f568d13fb6f22793f1c27959a843ae0d92
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2019-06-27 11:16:37 +0200

    Add cell assignment test

---
+++ tests/test_all_cells_assigned.cpp
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2017-2019 Steffen Hirschmann
+ *
+ * This file is part of Repa.
+ *
+ * Repa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Repa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Repa.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Tests if all processes agree on pos_to_rank
+ */
+
+#define BOOST_TEST_MODULE all_cells_assigned
+#include <boost/test/included/unit_test.hpp>
+
+#include "testenv.hpp"
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
+#include <random>
+#include <repa/repa.hpp>
+
+boost::mpi::environment env;
+
+static void
+test_exactly_one_assigned_process(const boost::mpi::communicator &comm,
+                                  repa::grids::ParallelLCGrid *grid,
+                                  const repa::Vec3d &opos)
+{
+    repa::Vec3d pos{opos}; // Create copy since methods expect "double *", not
+                           // "const double *"
+
+    int cellidx;
+    bool has_cell = false;
+    try {
+        cellidx = grid->position_to_cell_index(pos.data());
+        has_cell = true;
+    }
+    catch (...) {
+    }
+
+    // Ensure a process claiming to be responsibe is indeed the owner.
+    if (has_cell) {
+        BOOST_TEST(((0 <= cellidx) && (cellidx < grid->n_local_cells())));
+    }
+
+    int nresp
+        = boost::mpi::all_reduce(comm, has_cell ? 1 : 0, std::plus<int>{});
+    BOOST_TEST(nresp == 1);
+}
+
+static void test(const boost::mpi::communicator &comm,
+                 const repa::Vec3d &box,
+                 repa::grids::ParallelLCGrid *grid)
+{
+    // Test reguar grid
+    auto cell_size = grid->cell_size();
+    repa::Vec3d pos;
+
+    for (pos[0] = .5 * cell_size[0]; pos[0] < box[0]; pos[0] += cell_size[0]) {
+        for (pos[1] = .5 * cell_size[1]; pos[1] < box[1];
+             pos[1] += cell_size[1]) {
+            for (pos[2] = .5 * cell_size[2]; pos[2] < box[2];
+                 pos[2] += cell_size[2]) {
+                test_exactly_one_assigned_process(comm, grid, pos);
+            }
+        }
+    }
+}
+
+// Gridbased and Diffusion do not allow for position_to_rank after
+// repartitioning, so test statically.
+BOOST_AUTO_TEST_CASE(test_all_cells_assigned)
+{
+    using std::placeholders::_1;
+    boost::mpi::communicator comm;
+    repa::Vec3d box = {{20., 20., 20.}};
+    double mings = 1.0;
+    new_test_env(comm, box, mings)
+        .with_repart()
+        .all_grids()
+        .run(std::bind(test, std::cref(comm), std::cref(box), _1));
+}
