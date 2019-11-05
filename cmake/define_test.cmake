3b42690b98891c5da2ef79b3ccd25011b7536307
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2019-11-05 13:54:12 +0100

    Remove --oversubscribe for compatibility

--- cmake/define_test.cmake
+++ cmake/define_test.cmake
@@ -50,7 +50,7 @@ function(define_test)
     endif(TEST_SINGLEPROC)
     foreach(nproc RANGE 1 ${TEST_UPPER_BOUND} 1)
         add_test(NAME "${TEST_NAME}-${nproc}"
-                 COMMAND ${MPIEXEC} "--oversubscribe"
+                 COMMAND ${MPIEXEC}
                          ${MPIEXEC_NUMPROC_FLAG} ${nproc}
                          ${MPIEXEC_PREFLAGS} ${TEST_NAME} ${MPIEXEC_POSTFLAGS})
     endforeach()
