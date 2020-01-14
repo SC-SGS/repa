970c7e06a2ff1cc803c95193f1f4921c119045f5
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2020-01-14 14:56:41 +0100

    Switch diffusion to Cartesian1D initialization

--- repa/grids/diffusion.cpp
+++ repa/grids/diffusion.cpp
@@ -267,7 +267,7 @@ Diffusion::Diffusion(const boost::mpi::communicator &comm,
     // Initial partitioning
     partition.resize(gbox.ncells());
     util::InitPartitioning{gbox, comm_cart}(
-        util::InitialPartitionType::LINEAR,
+        util::InitialPartitionType::CARTESIAN1D,
         [this](global_cell_index_type idx, rank_type r) {
             assert(r >= 0 && r < this->comm.size());
             this->partition[idx] = r;
