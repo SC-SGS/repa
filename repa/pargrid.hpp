8c2f073f620a63ce8605b4536cc3ed4f33cbe812
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2020-02-12 10:24:50 +0100

    Properly document current behavior
    std::domain_error from position_to_neighidx means either not in ghost layer
    or not on any neighboring process.

--- repa/pargrid.hpp
+++ repa/pargrid.hpp
@@ -197,9 +197,16 @@ struct ParallelLCGrid {
     virtual rank_type position_to_rank(Vec3d pos) = 0;
 
     /** Returns the index of a neighboring process which is responsible for the
-     * cell at position "pos". "Pos" must therefore be *in the ghost layer*.
+     * cell at position "pos".
+     * The implementation might require, that "pos" is in the ghost layer of
+     * this process *OR*---a more relaxed condition---is on any neighboring
+     * process. In any case, the position cannot be resolved and throws a
+     * std::domain_error. As a consequence, the user *MUST NOT* rely on this
+     * method throwing a std::domain_error means that the position is not in a
+     * ghost layer.
      *
-     * @throws std::domain_error if position is not in the ghost layer.
+     * @throws std::domain_error If position cannot be resolved by this process.
+     *                           See comment above!
      */
     virtual rank_index_type position_to_neighidx(Vec3d pos) = 0;
 
