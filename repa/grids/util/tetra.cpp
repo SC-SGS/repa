636a3eafd81b0b59528361349e63e6a34d0a139b
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2020-05-18 13:49:43 +0200

    Exchange loops

--- repa/grids/util/tetra.cpp
+++ repa/grids/util/tetra.cpp
@@ -63,8 +63,8 @@ std::pair<Vec3i64, Vec3i64> min_max_per_dim(const Vertices &vertices)
 {
     Vec3i64 min = box_size;
     Vec3i64 max{0, 0, 0};
-    for (int d = 0; d < 3; d++) {
-        for (const Vec3i64 &vertex : vertices) {
+    for (const Vec3i64 &vertex : vertices) {
+        for (int d = 0; d < 3; d++) {
             min[d] = std::min(vertex[d], min[d]);
             max[d] = std::max(vertex[d], max[d]);
         }
