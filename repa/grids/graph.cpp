SC-SGS/repa

commit d7c09f6b15ca65268d904bc89c7e69f9e38bdbcc

Fix Graph Partitioner overflow
This commit fixes an edge case where vtxdist[size - 1] >= nglobalcells.

repa/grids/graph.cpp
@@ -61,6 +61,16 @@ Graph::~Graph()
 {
 }
 
+constexpr inline double clamp(double min, double val, double max)
+{
+    if (val < min)
+        return min;
+    else if (val > max)
+        return max;
+    else
+        return val;
+}
+
 /*
  * Repartition.
  * Every node is responsible for a certain range of cells along the
@@ -81,8 +91,10 @@ bool Graph::sub_repartition(CellMetric m, CellCellMetric ccm)
 
     // Vertex ranges per process
     std::vector<idx_t> vtxdist(comm_cart.size() + 1);
-    for (rank_type i = 0; i < comm_cart.size(); ++i)
-        vtxdist[i] = i * ncells_per_proc;
+    for (rank_type i = 0; i < comm_cart.size(); ++i) {
+        idx_t first_index = i * ncells_per_proc;
+        vtxdist[i] = clamp(0, first_index, nglocells - 1);
+    }
     vtxdist[comm_cart.size()] = nglocells;
 
 #ifdef GRAPH_DEBUG

commit 4b23f028d3785f647267a580a0915dc2f25fc261

Fix regression in graph partitioning
Changes introduced in commit d7c09f6b broke inverse mapping.
This commit creates a new fail-safe inverse mapping.

repa/grids/graph.cpp
@@ -71,6 +71,54 @@ constexpr inline double clamp(double min, double val, double max)
         return val;
 }
 
+namespace {
+/** Searches in range [begin, end) for an element greater than the specified.
+ * Behaves like a call so std::upper_bound. However, if called successively,
+ * this class assumes that the elements searched for monotonously increase.
+ */
+template <typename It>
+struct FwdSearchUpperBound {
+    using T = typename It::value_type;
+    FwdSearchUpperBound(It begin, It end) : _begin(begin), _end(end)
+    {
+    }
+
+    // Returns the iterator to the first element greater than "element".
+    // If no such exists, return "end".
+    It upper_bound(const T &element)
+    {
+#ifndef NDEBUG
+        // Check pre-condition
+        if (!_firstcall) {
+            assert(element >= _last_el);
+            _last_el = element;
+        }
+#endif
+        for (; _begin != _end; _begin++) {
+            if (*_begin > element)
+                return _begin;
+        }
+        return _end;
+    }
+
+private:
+    It _begin, _end;
+
+#ifndef NDEBUG
+    T _last_el = T{0};
+    bool _firstcall = true;
+#endif
+};
+
+template <typename Cont>
+FwdSearchUpperBound<typename Cont::const_iterator>
+new_fwd_searcher(const Cont &c)
+{
+    return FwdSearchUpperBound<typename Cont::const_iterator>{std::cbegin(c),
+                                                              std::cend(c)};
+}
+} // namespace
+
 /*
  * Repartition.
  * Every node is responsible for a certain range of cells along the
@@ -136,11 +184,26 @@ bool Graph::sub_repartition(CellMetric m, CellCellMetric ccm)
     // Sending vertex weights
     std::vector<boost::mpi::request> sreq;
     std::vector<std::vector<Weights>> my_weights(comm_cart.size());
+
+    auto gp_part = new_fwd_searcher(vtxdist);
     for (const auto i : cell_store.local_cells()) {
         // "Rank" is responsible for cell "gidx" / "i" (local)
         // during graph parititioning
         const global_cell_index_type gidx = cell_store.as_global_index(i);
-        const rank_type rank = gidx / ncells_per_proc;
+
+        // The inverse of cell-rank-assignment.
+        // Note that due to rounding and clamping this is not straightforward
+        // a simple "gidx / n_cells_per_proc". Therefore, we search for it
+        // in vtxdist. We don't use a binary search because of its logarithmic
+        // cost but rather use a linear forward search because the inputs
+        // ("gidx") are ordered/monotonously increasing.
+        // This makes the whole process (all upper_bound-calls) O(N+P)
+        // because "gp_part" traverses the vtxdist array concurrently to the
+        // traversal of cell_store.local_cells().
+        const auto it = gp_part.upper_bound(gidx);
+        assert(it != vtxdist.end());
+        assert(it != vtxdist.begin());
+        const rank_type rank = std::distance(vtxdist.cbegin(), it) - 1;
 
         Weights w;
         w[0] = static_cast<idx_t>(vertex_weights[i]);